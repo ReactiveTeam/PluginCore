8266fdcb9f1623791ce0ec7fe7bfc1dba5eeb0ff
Haikal Izzuddin <heywhite69@gmail.com>
2016-11-16 12:01:50 +0800

    Initial Commit

---
+++ IPlugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginCore
+{
+    /// <summary>
+    /// Plugin interface. Implement this in your class to allow it to be detected by the plugin manager.
+    /// </summary>
+    public interface IPlugin
+    {
+        #region Module Info
+        /// <summary>
+        /// The modules name
+        /// </summary>
+        string pluginName { get; }
+
+        /// <summary>
+        /// The modules description
+        /// </summary>
+        string pluginDescription { get; }
+
+        /// <summary>
+        /// The modules author
+        /// </summary>
+        string pluginAuthor { get; }
+        #endregion
+
+        void Start();
+        void Stop();
+        void Tick();
+    }
+}
